2f7c591ce6d2e1719c03a5e2da934409f94c6f22
Vicente Junior <vicente19981@live.com>
2023-03-31 22:47:53 -0300

    feat: create team model

--- app/models.py
+++ app/models.py
@@ -6,3 +6,13 @@ from django.db import models
 class Player(models.Model):
     name = models.CharField(max_length=100)
     initial_price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
