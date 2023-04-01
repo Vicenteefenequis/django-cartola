b113e4f6d47aba3bfab002066ade923821155064
Vicente Junior <vicente19981@live.com>
2023-03-31 22:58:22 -0300

    feat: create match model

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Player, Team, MyTeam
+from .models import Player, Team, MyTeam, Match
 # Register your models here.
 
 
 admin.site.register(Player)
 admin.site.register(Team)
 admin.site.register(MyTeam)
+admin.site.register(Match)
